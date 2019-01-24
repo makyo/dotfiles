7329e8b69a54d5a43565f36bbdb7dd08993b903a
Madison Scott-Clary <madison@archive.org>
2019-01-24 12:16:09 -0800

    Add fish helpers

---
+++ shell/helpers.fish
@@ -0,0 +1,21 @@
+function sshwg
+        ssh -A madison@sshgw-sf.us.archive.org
+end
+
+function devbox
+        ssh -A madison@wbgrp-svc110.us.archive.org
+end
+
+function mount-devbox
+        umount -f ~/work/devbox-work
+        sshfs madison@wbgrp-svc110.us.archive.org:/home/madison/work ~/work/devbox-work
+end
+
+function proxyhome
+        #sshuttle -r madison@sshgw-sf.us.archive.org 0/0
+        sshuttle -r madison@sshgw-sf.us.archive.org 207.241.224.0/20
+end
+
+function ps-
+        ps -HFe
+end
