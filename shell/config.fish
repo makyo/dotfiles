6efad96b133ec7b8e2a55228c95dc19184884677
Madison Scott-Clary <makyo@drab-makyo.com>
2020-05-09 18:15:42 -0700

    More vim modules, zk stuff

--- shell/config.fish
+++ shell/config.fish
@@ -1,6 +1,6 @@
 if status is-interactive
         set fish_key_bindings fish_vi_key_bindings
-        set -x GOPATH "$HOME/work/go"
+        set -x GOPATH "/home/makyo/work/go"
         set -x PATH $PATH ~/bin
         if test -d $GOPATH/bin
                 set -x PATH $PATH $GOPATH/bin
@@ -16,7 +16,7 @@ if status is-interactive
         set -x JUJU_REPOSITORY /home/makyo/work/charms
         set -x LAYER_PATH $JUJU_REPOSITORY/layers
         set -x INTERFACE_PATH $JUJU_REPOSITORY/interfaces
-        set -x VIMWIKI_MARKDOWN_EXTENSIONS markdown_vimwiki,markdown_verse,markdown_editing,toc,wikilinks,extra
+        set -x VIMWIKI_MARKDOWN_EXTENSIONS markdown_vimwiki,markdown_verse,markdown_editing,toc,wikilinks,extra,smarty
         set -l GPG_AGENT_INFO (gpg-agent -q --daemon 2>&1 | cut -d '=' -f 2 | cut -d ';' -f 1)
         source ~/.config/fish/functions/fish_prompt.fish
         source ~/.config/fish/functions/helpers.fish
